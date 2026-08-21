550c00f4e33b5fa98eee39d28feee742fa5d2c85
miladnaji123 <34491327+miladnaji123@users.noreply.github.com>
2026-08-21 18:24:21 +0330

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,3 @@
-const express = require("express");
-const http = require("http");
-
-const app = express();
-const server = http.createServer(app);
-
-const PORT = process.env.PORT || 10000;
-
-app.get("/", (req, res) => {
-    res.status(200).send("🎲 Milad Game Server is ONLINE!");
-});
-
 app.get("/health", (req, res) => {
     res.status(200).json({
         status: "ok",
@@ -17,7 +5,6 @@ app.get("/health", (req, res) => {
     });
 });
 
-server.listen(PORT, "0.0.0.0", () => {
-    console.log("🎲 Milad Game Server Started");
-    console.log("PORT:", PORT);
+app.get("/health1", (req, res) => {
+    res.status(200).send("OK");
 });
